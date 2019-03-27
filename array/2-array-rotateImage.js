e05c6d4319899dd3303ca4e8cb7ff48a05657c51
Kevin Bai <lkevin957@gmail.com>
2019-03-26 21:36:50 -0700

    add rotateImage

---
+++ array/2-array-rotateImage.js
@@ -0,0 +1,11 @@
+// O(1) space
+
+function rotateImage(a) {
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            a[i].unshift( a.shift() )
+        }
+    }
+
+    return a;
+}
