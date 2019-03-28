a062af37253fb0a7dc1bf1b020df188366183314
Kevin Bai <lkevin957@gmail.com>
2019-03-28 09:50:59 -0700

    Add 3 array sudoku problem

---
+++ array/3-array-sudoku.js
@@ -0,0 +1,32 @@
+// objective is to fill a 9 × 9 grid with numbers in such a way that
+    // each column, row, and 3 x 3 subgrid contains the numbers 1 - 9
+
+// Input: 9x9 2D array
+// Output: boolean determining if this is a valid sudoku puzzle
+
+function sudoku2(grid) {
+    let rowChecker = new Set();
+    let columnChecker = new Set();;
+    let subGridChecker;
+
+    for (let i = 0; i < grid.length; i++) {
+        const row = array[i];
+
+        
+    }
+}
+
+func
+
+grid = [
+    ['.', '.', '.', '1', '4', '.', '.', '2', '.'],
+    ['.', '.', '6', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '1', '.', '.', '.', '.', '.', '.'],
+    ['.', '6', '7', '.', '.', '.', '.', '.', '9'],
+    ['.', '.', '.', '.', '.', '.', '8', '1', '.'],
+    ['.', '3', '.', '.', '.', '.', '.', '.', '6'],
+    ['.', '.', '.', '.', '.', '7', '.', '.', '.'],
+    ['.', '.', '.', '5', '.', '.', '.', '7', '.']
+]
+
