5beb863b59317aaf90c0f5999f3622e8a08d18ae
Kevin Bai <lkevin957@gmail.com>
2019-04-01 15:15:34 -0700

    get removek operational

--- linkedList/0-linkedList-removeKFromList.js
+++ linkedList/0-linkedList-removeKFromList.js
@@ -1,7 +1,26 @@
 // Restrictions: O(n) time, O(1) space
-// Input: singly linked list of integers starting from 1 and an integer k
+// Input: singly linked list of integers starting and an integer k
 // Output: remove all elements from list 1 with a value of k
 
-function removeKFromList(l, k) {
+function removeKFromList(list, kDelete) {
+    let newList = [];
 
+    if (!list) return newList;
+    if (!list.length) return newList;
+
+
+    for (let i = 0; i < list.length; i++){
+        debugger;
+        let number = list[i];
+        if (number !== kDelete) {
+            newList.push(number)
+        }
+    }
+
+    return newList;
 }
+
+let l = [3, 1, 2, 3, 4, 5];
+let k = 3;
+
+removeKFromList(l, k)
