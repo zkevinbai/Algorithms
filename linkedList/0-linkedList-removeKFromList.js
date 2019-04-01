c5947c8cad276196e60a4c519ac45fcc1a28427e
Kevin Bai <lkevin957@gmail.com>
2019-04-01 14:52:04 -0700

    Add remove k from linked list

---
+++ linkedList/0-linkedList-removeKFromList.js
@@ -0,0 +1,7 @@
+// Restrictions: O(n) time, O(1) space
+// Input: singly linked list of integers starting from 1 and an integer k
+// Output: remove all elements from list 1 with a value of k
+
+function removeKFromList(l, k) {
+
+}
