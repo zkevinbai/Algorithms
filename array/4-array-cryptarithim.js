8e8affa50af272fc12940caf4402612e7ee46fc1
Kevin Bai <lkevin957@gmail.com>
2019-04-01 10:26:54 -0700

    complete array cryptarithim

--- array/4-array-cryptarithim.js
+++ array/4-array-cryptarithim.js
@@ -42,15 +42,15 @@ function encrypt(crypt, solution) {
     return encrypted;
 }
 
-let crypt = ["SEND", "MORE", "MONEY"]
+// let crypt = ["SEND", "MORE", "MONEY"]
 
-let solution = [
-    ["O", "0"],
-    ["M", "1"],
-    ["Y", "2"],
-    ["E", "5"],
-    ["N", "6"],
-    ["D", "7"],
-    ["R", "8"],
-    ["S", "9"]
-]
+// let solution = [
+//     ["O", "0"],
+//     ["M", "1"],
+//     ["Y", "2"],
+//     ["E", "5"],
+//     ["N", "6"],
+//     ["D", "7"],
+//     ["R", "8"],
+//     ["S", "9"]
+// ]
