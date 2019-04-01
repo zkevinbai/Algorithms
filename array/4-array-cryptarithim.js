a0c1bd7281daced7f370ede137b68a16f16faa9a
Kevin Bai <lkevin957@gmail.com>
2019-04-01 10:10:21 -0700

    add array cryptarithim

---
+++ array/4-array-cryptarithim.js
@@ -0,0 +1,3 @@
+// Input: 3 words in an array called crypt, a 2d array dictionary called solution
+// Output: if the 3 words, once converted to numbers are valid numbers and
+//         if the first 2 words add up to equal the 3rd word, return true, else false
