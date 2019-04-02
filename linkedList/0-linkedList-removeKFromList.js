4be2f81166a70000e109eb9e05c0876af0c15b12
Kevin Bai <lkevin957@gmail.com>
2019-04-02 08:44:30 -0700

    Refactor to be closer to pointer pattern

--- linkedList/0-linkedList-removeKFromList.js
+++ linkedList/0-linkedList-removeKFromList.js
@@ -2,25 +2,41 @@
 // Input: singly linked list of integers starting and an integer k
 // Output: remove all elements from list 1 with a value of k
 
+
 function removeKFromList(list, kDelete) {
-    let newList = [];
+    if (!list) return [];
+    let head = list;
+    let pointerA = list;
+    let pointerB = list.next;
 
-    if (!list) return newList;
-    if (!list.length) return newList;
-
-
-    for (let i = 0; i < list.length; i++){
-        debugger;
-        let number = list[i];
-        if (number !== kDelete) {
-            newList.push(number)
-        }
+    if (list.value === kDelete) {
+        head = pointerB;
+        pointerA.next = null;
     }
 
-    return newList;
+    return head;
 }
 
-let l = [3, 1, 2, 3, 4, 5];
-let k = 3;
+// For array list
+// function removeKFromList(list, kDelete) {
+//     let newList = [];
 
-removeKFromList(l, k)
+//     if (!list) return newList;
+//     if (!list.length) return newList;
+
+
+//     for (let i = 0; i < list.length; i++){
+//         debugger;
+//         let number = list[i];
+//         if (number !== kDelete) {
+//             newList.push(number)
+//         }
+//     }
+
+//     return newList;
+// }
+
+// let l = [123, 456, 789, 0]
+// let k = 0;
+
+// removeKFromList(l, k)
