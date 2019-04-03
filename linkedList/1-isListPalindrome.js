81ecc71642e0d880facc51bdbd48f03e646d8513
Kevin Bai <lkevin957@gmail.com>
2019-04-03 14:52:03 -0700

    Update list palindrome with 2 pointer strategy

--- linkedList/1-isListPalindrome.js
+++ linkedList/1-isListPalindrome.js
@@ -6,22 +6,66 @@
 // Restrictions O(n) time, using O(1) space
 
 // strategy
-    // get the length, and know where half is
-    // create a stack
+    // split in half
+    // reverse the first half of the list in place
+    // 2 iterations to check if values are equal
 
 function isListPalindrome(list) {
-    let array = [];
     if (!list) return array;
     let node = list;
     let listLength = 0;
 
     while (node) {
-        array.push(node.value);
         node = node.next;
         listLength += 1;
     }
 
-    let rev = array.slice();
-    rev = rev.reverse();
-    return array.toString() === rev.toString();
+    let firstHalfHead;
+    let secondHalfHead;
+
+    let lead = list.next; //b (.next is c)
+    let trail = list;   //a (.next is b)
+    for (let i = 0; i < Math.floor(listLength / 2); i++) {
+        if (i === Math.floor(listLength / 2) - 1) {
+            firstHalfHead = trail;
+            secondHalfHead = lead;
+        } else {
+            trail.next = null;
+            let temp = lead.next; //c
+            lead.next = trail; //b.next = a
+            trail = lead;   //b
+            lead = temp;    //c
+        }
+    }
+
+    let pointerA = firstHalfHead;
+    let pointerB = secondHalfHead;
+    return [pointerA, pointerB]
+    for (let i = 0; i <= Math.floor(listLength / 2); i++) {
+        if (pointerA.value !== pointerB.value) {
+            return false;
+        }
+        pointerA = pointerA.next;
+        pointerB = pointerB.next;
+    }
+
+    return true;
 }
+
+// O(n) space O(n) time
+// function isListPalindrome(list) {
+//     let array = [];
+//     if (!list) return array;
+//     let node = list;
+//     let listLength = 0;
+
+//     while (node) {
+//         array.push(node.value);
+//         node = node.next;
+//         listLength += 1;
+//     }
+
+//     let rev = array;
+//     rev = rev.reverse();
+//     return array.toString() === rev.toString();
+// }
