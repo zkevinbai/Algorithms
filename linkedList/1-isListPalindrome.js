b7ec40a577de5468d40ffb58888b4021827ee5e4
Kevin Bai <lkevin957@gmail.com>
2019-04-03 13:45:47 -0700

    Update is list palindrome

--- linkedList/1-isListPalindrome.js
+++ linkedList/1-isListPalindrome.js
@@ -1,10 +1,22 @@
-function isListPalindrome(l) {
+// Input: 
+    // The first element of a node list (with next and value properties)
+// Output:
+    // boolean indicating whether this list is a palindrome
+    
+// Restrictions O(n) time, using O(1) space
+
+function isListPalindrome(list) {
     let array = [];
-    let node = list.value;
+    if(!list) return array;
+    let node = list;
     while(node) {
-        array.push(node);
+        array.push(node.value);
         node = node.next;
     }
 
-    return array === array.reverse();
+    let rev = array;
+    rev = rev.reverse();
+    return [arr, rev];
+    return array === rev;
 }
+
