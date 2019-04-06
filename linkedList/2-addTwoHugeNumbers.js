d6f3a04366b8bd2671ca9257efac45f211713b9c
Kevin Bai <lkevin957@gmail.com>
2019-04-06 15:48:18 -0700

    add logic to remove exponents

--- linkedList/2-addTwoHugeNumbers.js
+++ linkedList/2-addTwoHugeNumbers.js
@@ -1,13 +1,40 @@
+// Definition for singly-linked list:
 // function ListNode(x) {
 //   this.value = x;
 //   this.next = null;
 // }
+//
+
+// input: 
+    // 2 large numbers represented as a linked list
+    // each link is a 4 digit number with leading zeros removed
+// output
+    // sum of 2 large numbers in linkedlist or array it looks like
+    // segmented into 4 digits, with leading zeros removed
+
+Number.prototype.noExponents = function () {
+    var data = String(this).split(/[eE]/);
+    if (data.length == 1) return data[0];
+
+    var z = '', sign = this < 0 ? '-' : '',
+        str = data[0].replace('.', ''),
+        mag = Number(data[1]) + 1;
+
+    if (mag < 0) {
+        z = sign + '0.';
+        while (mag++) z += '0';
+        return z + str.replace(/^\-/, '');
+    }
+    mag -= str.length;
+    while (mag--) z += '0';
+    return str + z;
+}
 
 function addTwoHugeNumbers(a, b) {
     let aValue = getValue(a);
     let bValue = getValue(b);
 
-    let newValue = aValue + bValue + "";
+    let newValue = (aValue + bValue).noExponents() + "";
 
     let fourSplit = [];
 
@@ -27,15 +54,14 @@ function addTwoHugeNumbers(a, b) {
         newValue = newValue.slice(0, startShave);
     }
 
-    return fourSplit
     return fourSplit.map(element => parseInt(element));
 }
 
-function linkedListMaker(array){
+function linkedListMaker(array) {
     let shift = array.shift();
     let node = ListNode(parseInt(shift));
 
-    while(array.length) {
+    while (array.length) {
         shift = array.shift();
 
         let temp = ListNode(parseInt(shift));
@@ -47,7 +73,6 @@ function linkedListMaker(array){
 
     return node;
 }
-
 
 function shaveLeadingZeros(string) {
     while (string[0] === "0") {
