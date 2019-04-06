352ae62ce557652ddac3b45d240f6b0cbaa901dc
Kevin Bai <lkevin957@gmail.com>
2019-04-06 15:28:49 -0700

    Add function to shave leading zeros

--- linkedList/2-addTwoHugeNumbers.js
+++ linkedList/2-addTwoHugeNumbers.js
@@ -1,9 +1,35 @@
+// function ListNode(x) {
+//   this.value = x;
+//   this.next = null;
+// }
+
 function addTwoHugeNumbers(a, b) {
     let aValue = getValue(a);
     let bValue = getValue(b);
 
     let newValue = aValue + bValue + "";
-    return newValue;
+
+    let fourSplit = [];
+
+    while(newValue.length) {
+        fourSplit.push(
+            shaveLeadingZeros(newValue.slice(0,4))
+        );
+
+        newValue = newValue.slice(4);
+    }
+
+    return fourSplit;
+}
+
+function shaveLeadingZeros(string) {
+    while (string[0] === "0") {
+        string = string.slice(1);
+    }
+
+    if (!string.length) return "0";
+
+    return string;
 }
 
 function getValue(linkList) {
