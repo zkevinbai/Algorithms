8b93f5f029927a5c226754667b10967e4d79bbc4
Kevin Bai <lkevin957@gmail.com>
2019-04-10 11:32:37 -0700

    Add logic to handle if a list does not have a next

--- CodeSignal/linkedList/3-mergeTwoLinkedLists.js
+++ CodeSignal/linkedList/3-mergeTwoLinkedLists.js
@@ -8,29 +8,40 @@
 // Strategy
     // merge sort but with lists instead of arrays
 
-
 function mergeTwoLinkedLists(l1, l2) {
 
-    let listOne = l1; 
-    let listTwo = l2; 
+    let listOne = l1;
+    let listTwo = l2;
 
+    let node;
     if (listOne.value < listTwo.value) {
-        let node = new ListNode(listOne.value);
+        node = new ListNode(listOne.value);
         listOne = listOne.next;
     } else {
-        let node = new ListNode(listTwo.value);
+        node = new ListNode(listTwo.value);
         listTwo = listTwo.next;
     }
 
     let head = node;
 
-    while(listOne && listTwo){
+    while (listOne && listTwo) {
         if (listOne.value < listTwo.value) {
             node.next = listOne.value;
-            listOne = listOne.next;
+            if (!listOne.next) {
+                listOne = null
+            } else {
+                listOne = listOne.next;
+            }
+
         } else {
             node.next = listTwo.value;
             listTwo = listTwo.next;
+
+            if (!listTwo.next) {
+                listTwo = null
+            } else {
+                listTwo = listTwo.next;
+            }
         }
 
         node = node.next;
