e78a6069fcbcb5844d81a61c374b0e1ec51433cf
Kevin Bai <lkevin957@gmail.com>
2019-04-10 11:27:16 -0700

    Add merge 2 linked lists logic

--- CodeSignal/linkedList/3-mergeTwoLinkedLists.js
+++ CodeSignal/linkedList/3-mergeTwoLinkedLists.js
@@ -4,6 +4,43 @@
 //   this.next = null;
 // }
 //
+
+// Strategy
+    // merge sort but with lists instead of arrays
+
+
 function mergeTwoLinkedLists(l1, l2) {
 
+    let listOne = l1; 
+    let listTwo = l2; 
+
+    if (listOne.value < listTwo.value) {
+        let node = new ListNode(listOne.value);
+        listOne = listOne.next;
+    } else {
+        let node = new ListNode(listTwo.value);
+        listTwo = listTwo.next;
+    }
+
+    let head = node;
+
+    while(listOne && listTwo){
+        if (listOne.value < listTwo.value) {
+            node.next = listOne.value;
+            listOne = listOne.next;
+        } else {
+            node.next = listTwo.value;
+            listTwo = listTwo.next;
+        }
+
+        node = node.next;
+    }
+
+    if (listOne) {
+        node.next = listOne;
+    } else if (listTwo) {
+        node.next = listTwo;
+    }
+
+    return head;
 }
