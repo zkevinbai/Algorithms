83817625a912f1ee902fb134a51360ec84234b46
Kevin Bai <lkevin957@gmail.com>
2019-04-12 15:41:06 -0700

    Add notes

--- CodeSignal/linkedList/4-reverseNodeInKGroups.js
+++ CodeSignal/linkedList/4-reverseNodeInKGroups.js
@@ -4,6 +4,20 @@
 //   this.next = null;
 // }
 //
+
+// Input
+    // linked list l
+    // k is a positive integer less than or equal to the number of nodes in linked list
+
+// Output
+    // same list with nodes reversed k at at a time
+    // return modified linked list
+    // if linked list is not a multiple of k, the nodes at the end should be left as is
+
+// Restrictions
+    // O(n) time
+    // O(1) space
+
 function reverseNodesInKGroups(l, k) {
 
 }
