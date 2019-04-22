85902832897bcbe9d080b9ac4f47226412f059ca
Kevin Bai <lkevin957@gmail.com>
2019-04-21 18:26:09 -0700

    Add segment logic

--- CodeSignal/linkedList/4-reverseNodeInKGroups.js
+++ CodeSignal/linkedList/4-reverseNodeInKGroups.js
@@ -23,19 +23,61 @@
     // reverseNodesInKGroups(l, k) = [2, 1, 4, 3, 5];
 
 // strategy
-    // create function to reverse any linked list
-    // create function to segment a list into k sections
-    // build reversed linked list using the function
+    // [x] create function to reverse any linked list
+    // [x] create function to segment a list into k sections
+    // [] create function to determine the length of a linked list;
+    // [] build reversed linked list using the function
 
 function reverseNodesInKGroups(l, k) {
     // base case
     if(k < 2) return l;
 
     // build segments of linked list
-    segmentLinkedList(l, k)
+    let segments = segmentLinkedList(l, k);
+
+    // build new linked list, reverse only if sub linked list is of k length;
+
+
 }
 
-// reverse entire linked list;
+// segment linked list
+function segmentLinkedList(l, k) {
+    let segments = [];
+    let node = l;
+    let counter = 0;
+
+    while (node) {
+        if (counter === 0) {
+            segments.push(node);
+            counter = k;
+        }
+
+        counter -= 1;
+        node = node.next;
+    }
+
+    for (let i = 0; i < segments.length; i++) {
+        const segment = segments[i];
+
+        let counter = k;
+        let node = segment;
+        let copy = node;
+
+        while (counter > 1) {
+            if (node.next) {
+                node = node.next;
+            }
+            counter -= 1;
+        }
+        node.next = null;
+
+        segments[i] = copy;
+    }
+
+    return segments;
+}
+
+// reverse linked list;
 function linkedListReverse(l) {
     let prev = null;
     let current = l;
@@ -55,22 +97,4 @@ function linkedListReverse(l) {
     }
 
     return newHead;
-}
-
-function segmentLinkedList(l, k) {
-    let segments = [];
-    let node = l;
-    let counter = 0;
-
-    while (node) {
-        if (counter === 0) {
-            segments.push(node);
-            counter = k;
-        }
-
-        counter -= 1;
-        node = node.next;
-    }
-
-    return segments;
 }
