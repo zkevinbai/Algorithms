cc629ef9d777c968ad2f6a5e9c1640459f371a9e
Kevin Bai <lkevin957@gmail.com>
2019-04-22 19:47:26 -0700

    add rearrange last n

---
+++ CodeSignal/linkedList/5-rearrangeLastN.js
@@ -0,0 +1,22 @@
+// Definition for singly-linked list:
+// function ListNode(x) {
+//   this.value = x;
+//   this.next = null;
+// }
+//
+
+// Input
+    // linked list
+    // non negative integer n
+
+// Output
+    // linked list with the lead being the last n elements of the input list
+    // and terminiating at the n-1th position from the back of the input list
+
+// Example
+    // i: [0,1,2,3,4], 2
+    // o: [3, 4, 0,1,2]
+
+function rearrangeLastN(l, n) {
+
+}
