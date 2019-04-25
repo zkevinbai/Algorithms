959fa476f527ad290790b1d1562b0ed3bd25fb6f
Kevin Bai <lkevin957@gmail.com>
2019-04-24 19:50:18 -0700

    Refactor code for a faster solve

--- CodeSignal/hashTable/1-areFollowingPatterns.js
+++ CodeSignal/hashTable/1-areFollowingPatterns.js
@@ -10,34 +10,61 @@
         // confirm that the boolean result of that equality matches
         // the input string
 
+// function areFollowingPatterns(strings, patterns) {
+//     let indices = [];
+
+//     for (let i = 0; i < patterns.length; i++) {
+//         for (let j = i + 1; j < patterns.length; j++) {
+//             indices.push([i, j])
+//         }
+//     }
+
+//     for (let i = 0; i < indices.length; i++) {
+//         let indexArray = indices[i];
+//         let indexLeft = indexArray[0];
+//         let indexRight = indexArray[1];
+
+//         let patternsBoolean = false;
+
+//         if (patterns[indexLeft] === patterns[indexRight]) {
+//             patternsBoolean = true;
+//         } 
+
+//         let stringsBoolean = false;
+
+//         if (strings[indexLeft] === strings[indexRight]) {
+//             stringsBoolean = true;
+//         } 
+
+//         if (stringsBoolean !== patternsBoolean) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }   
+
+
 function areFollowingPatterns(strings, patterns) {
     let indices = [];
 
     for (let i = 0; i < patterns.length; i++) {
         for (let j = i + 1; j < patterns.length; j++) {
-            indices.push([i, j])
-        }
-    }
+            let pattern = false;
 
-    for (let i = 0; i < indices.length; i++) {
-        let indexArray = indices[i];
-        let indexLeft = indexArray[0];
-        let indexRight = indexArray[1];
+            if (patterns[i] === patterns[j]){
+                pattern = true;
+            }
 
-        let patternsBoolean = false;
+            let stringPattern = false;
 
-        if (patterns[indexLeft] === patterns[indexRight]) {
-            patternsBoolean = true;
-        } 
+            if (strings[i] === strings[j]) {
+                stringPattern = true;
+            }
 
-        let stringsBoolean = false;
-
-        if (strings[indexLeft] === strings[indexRight]) {
-            stringsBoolean = true;
-        } 
-
-        if (stringsBoolean !== patternsBoolean) {
-            return false;
+            if (stringPattern !== pattern){
+                return false;
+            }
         }
     }
 
