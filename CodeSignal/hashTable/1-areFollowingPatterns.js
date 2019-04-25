5ce9b8192676c6701b6e6a4804fd403dafe3478c
Kevin Bai <lkevin957@gmail.com>
2019-04-24 19:15:46 -0700

    Add initial logic

--- CodeSignal/hashTable/1-areFollowingPatterns.js
+++ CodeSignal/hashTable/1-areFollowingPatterns.js
@@ -9,5 +9,39 @@
         // the input string
 
 function areFollowingPatterns(strings, patterns) {
+    let indices = [];
 
-}
+    for (let i = 0; i < patterns.length; i++) {
+        for (let j = i+1; j < patterns.length; j++) {
+            indices.push([i,j])
+        }        
+    }
+
+    for (let i = 0; i < indices.length; i++) {
+        let indexArray = indices[i];
+        let indexLeft = indexArray[0];
+        let indexRight = indexArray[0];
+        
+        let patternsBoolean;
+
+        if(patterns[indexLeft] === patterns[indexRight]){
+            patternsBoolean = true;
+        } else {
+            patternsBoolean = false;
+        }
+
+        let stringsBoolean;
+        
+        if (strings[indexLeft] === strings[indexRight]) {
+            stringsBoolean = true;
+        } else {
+            stringsBoolean = false;
+        }
+
+        if(stringsBoolean !== patternsBoolean){
+            return false;
+        }
+    }
+
+    return true;
+}   
