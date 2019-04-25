54ed19feb73b3b0897edf95ff9b7043c98a25e70
Kevin Bai <lkevin957@gmail.com>
2019-04-24 19:17:36 -0700

    Debug issue with wrong index being accessed

--- CodeSignal/hashTable/1-areFollowingPatterns.js
+++ CodeSignal/hashTable/1-areFollowingPatterns.js
@@ -1,44 +1,44 @@
 // input
-    // an array of strings, an array of patterns
+// an array of strings, an array of patterns
 // output
-    // a boolean determining if the strings array follows the patterns array
+// a boolean determining if the strings array follows the patterns array
 // strategy
-    // generate all combinations of index positions
-    // check for equality in the pattern string
-        // confirm that the boolean result of that equality matches
-        // the input string
+// generate all combinations of index positions
+// check for equality in the pattern string
+// confirm that the boolean result of that equality matches
+// the input string
 
 function areFollowingPatterns(strings, patterns) {
     let indices = [];
 
     for (let i = 0; i < patterns.length; i++) {
-        for (let j = i+1; j < patterns.length; j++) {
-            indices.push([i,j])
-        }        
+        for (let j = i + 1; j < patterns.length; j++) {
+            indices.push([i, j])
+        }
     }
 
     for (let i = 0; i < indices.length; i++) {
         let indexArray = indices[i];
         let indexLeft = indexArray[0];
-        let indexRight = indexArray[0];
-        
+        let indexRight = indexArray[1];
+
         let patternsBoolean;
 
-        if(patterns[indexLeft] === patterns[indexRight]){
+        if (patterns[indexLeft] === patterns[indexRight]) {
             patternsBoolean = true;
         } else {
             patternsBoolean = false;
         }
 
         let stringsBoolean;
-        
+
         if (strings[indexLeft] === strings[indexRight]) {
             stringsBoolean = true;
         } else {
             stringsBoolean = false;
         }
 
-        if(stringsBoolean !== patternsBoolean){
+        if (stringsBoolean !== patternsBoolean) {
             return false;
         }
     }
