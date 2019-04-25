d4c1d29413361a9b32cd5a65d52063b7bc1736ec
Kevin Bai <lkevin957@gmail.com>
2019-04-24 19:07:43 -0700

    Add are following patterns

---
+++ CodeSignal/hashTable/1-areFollowingPatterns.js
@@ -0,0 +1,13 @@
+// input
+    // an array of strings, an array of patterns
+// output
+    // a boolean determining if the strings array follows the patterns array
+// strategy
+    // generate all combinations of index positions
+    // check for equality in the pattern string
+        // confirm that the boolean result of that equality matches
+        // the input string
+
+function areFollowingPatterns(strings, patterns) {
+
+}
