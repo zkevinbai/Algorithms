d4f01e2bc27e06563729b6caf2f6a5ea0297b373
Kevin Bai <lkevin957@gmail.com>
2019-04-24 19:30:56 -0700

    Create initial hashmap strategy

--- CodeSignal/hashTable/1-areFollowingPatterns.js
+++ CodeSignal/hashTable/1-areFollowingPatterns.js
@@ -10,36 +10,73 @@
         // confirm that the boolean result of that equality matches
         // the input string
 
+
 function areFollowingPatterns(strings, patterns) {
-    let indices = [];
+    let indices = {};
 
     for (let i = 0; i < patterns.length; i++) {
         for (let j = i + 1; j < patterns.length; j++) {
-            indices.push([i, j])
+            let patternsBoolean = false;
+
+            if (patterns[i] === patterns[j]) {
+                patternsBoolean = true;
+            }
+
+            indices[(i + "") + (j + "")] = patternsBoolean;
         }
     }
 
-    for (let i = 0; i < indices.length; i++) {
-        let indexArray = indices[i];
-        let indexLeft = indexArray[0];
-        let indexRight = indexArray[1];
+    let keys = Object.values(indices);
 
-        let patternsBoolean = false;
-
-        if (patterns[indexLeft] === patterns[indexRight]) {
-            patternsBoolean = true;
-        } 
+    for (let i = 0; i < keys.length; i++) {
+        let m = parseInt(keys[i][0]);
+        let n = parseInt(keys[i][1]);
 
         let stringsBoolean = false;
 
-        if (strings[indexLeft] === strings[indexRight]) {
+        if (strings[m] === strings[n]) {
             stringsBoolean = true;
-        } 
+        }
 
-        if (stringsBoolean !== patternsBoolean) {
+        if (stringsBoolean !== indices[keys[i]]){
             return false;
         }
     }
 
     return true;
 }   
+
+
+// function areFollowingPatterns(strings, patterns) {
+//     let indices = [];
+
+//     for (let i = 0; i < patterns.length; i++) {
+//         for (let j = i + 1; j < patterns.length; j++) {
+//             indices.push([i, j])
+//         }
+//     }
+
+//     for (let i = 0; i < indices.length; i++) {
+//         let indexArray = indices[i];
+//         let indexLeft = indexArray[0];
+//         let indexRight = indexArray[1];
+
+//         let patternsBoolean = false;
+
+//         if (patterns[indexLeft] === patterns[indexRight]) {
+//             patternsBoolean = true;
+//         } 
+
+//         let stringsBoolean = false;
+
+//         if (strings[indexLeft] === strings[indexRight]) {
+//             stringsBoolean = true;
+//         } 
+
+//         if (stringsBoolean !== patternsBoolean) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }   
