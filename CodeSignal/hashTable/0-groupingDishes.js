3612b54e0f1e9d6e282ca5483d5ffeabeb18812e
Kevin Bai <lkevin957@gmail.com>
2019-04-24 18:08:04 -0700

    Add Notes

---
+++ CodeSignal/hashTable/0-groupingDishes.js
@@ -0,0 +1,14 @@
+// Input
+    // 2d array of dishes and the ingredients used to make them
+        // the dish is the first entry in the sub array, the ingredients for that dish come after
+// Output
+    // 2d array of the ingredients and the dishes that they can be used to make
+        // the ingredient is the first entry in the sub array, the dishes come after
+        // only ingredients used to make two or more dishes are included
+
+function groupingDishes(dishes) {
+    let ingredients = {};
+
+
+
+}
