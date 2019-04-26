29dba6e7ea90ed5d469971f259d8582948605570
Kevin Bai <lkevin957@gmail.com>
2019-04-26 16:56:48 -0700

    Generate all indices

--- CodeSignal/hashTable/2-containsCloseNums.js
+++ CodeSignal/hashTable/2-containsCloseNums.js
@@ -6,6 +6,20 @@
         // where nums[i] = nums[j] and
         // the difference between i and j is less than or equal to k
 
-function containsCloseNums(nums, k) {
+// strategy
+    // generate all pairs of i and j, with k or less in between
 
+function containsCloseNums(nums, k) {
+    let indices = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            let difference = j - i;
+            if (difference <= k){
+                indices.push([i, j])
+            }
+        }
+    }
+
+    return indices;
 }
