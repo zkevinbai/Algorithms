5619d914919ca356b39960b3fec3115089dc3aee
Kevin Bai <lkevin957@gmail.com>
2019-04-26 16:54:21 -0700

    Add notes

---
+++ CodeSignal/hashTable/2-containsCloseNums.js
@@ -0,0 +1,11 @@
+// input
+    // array of integers nums, integer k
+
+// output
+    // boolean indicating there is a pair of key value pairs (i, j)
+        // where nums[i] = nums[j] and
+        // the difference between i and j is less than or equal to k
+
+function containsCloseNums(nums, k) {
+
+}
