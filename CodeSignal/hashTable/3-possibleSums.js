1073be46be1d9fba745ea4e5f1b0eca1bc70b8f6
Kevin Bai <lkevin957@gmail.com>
2019-04-26 19:09:48 -0700

    Add true count

--- CodeSignal/hashTable/3-possibleSums.js
+++ CodeSignal/hashTable/3-possibleSums.js
@@ -5,8 +5,18 @@
     // integer representing all distinct sums you can make from groupings
 
 // strategy
-    // create an array where each coin is represented n times, n being the coin count
+    // create a trueCount where each coin is represented n times, n being the coin count
 
 function possibleSums(coins, quantity) {
+    let trueCount = [];
+    while (quantity.length) {
+        let shift = quantity.shift();
+        let coinShift = coins.shift();
 
+        for (let i = 0; i < shift; i++) {
+            trueCount.push(coinShift);
+        }
+    }
+
+    return trueCount;
 }
