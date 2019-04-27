0020e78bd9b3a39cdb9424e2aca7e04f7e1a8ac2
Kevin Bai <lkevin957@gmail.com>
2019-04-26 19:03:11 -0700

    Add possible sums

---
+++ CodeSignal/hashTable/3-possibleSums.js
@@ -0,0 +1,9 @@
+// input
+    // coins array of coin values
+    // quantity array of number of each coin you have
+// output
+    // integer representing all distinct sums you can make from groupings
+
+function possibleSums(coins, quantity) {
+
+}
