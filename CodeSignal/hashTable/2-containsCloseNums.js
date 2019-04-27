ada6e4952fa04b6f88784a4955293444d1d6bc4f
Kevin Bai <lkevin957@gmail.com>
2019-04-26 17:11:16 -0700

    Achieve numbers hash

--- CodeSignal/hashTable/2-containsCloseNums.js
+++ CodeSignal/hashTable/2-containsCloseNums.js
@@ -8,27 +8,51 @@
 
 // strategy
     // generate all pairs of i and j, with k or less in between
+    // check the pairs for equality
+
+// SOLUTION ONE O(n^2)
+// function containsCloseNums(nums, k) {
+//     let indices = [];
+
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             let difference = j - i;
+//             if (difference <= k){
+//                 indices.push([i, j]);
+//             }
+//         }
+//     }
+
+//     for (let index = 0; index < indices.length; index++) {
+//         let i = indices[index][0];
+//         let j = indices[index][1];
+
+//         if (nums[i] === nums[j]){
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
+
+// strategy
+    // store all index positions of numbers in hashmap
+    // check if the index positions are length > 2, and the distance
+
+// SOLUTION TWO
 
 function containsCloseNums(nums, k) {
-    let indices = [];
+    let numbers = {};
 
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            let difference = j - i;
-            if (difference <= k){
-                indices.push([i, j]);
-            }
+        let number = nums[i];
+        if (numbers[number]) {
+            numbers[number].push(i);
+        } else {
+            numbers[number] = [i];
         }
     }
 
-    for (let index = 0; index < indices.length; index++) {
-        let i = indices[index][0];
-        let j = indices[index][1];
-
-        if (nums[i] === nums[j]){
-            return true;
-        }
-    }
-
-    return false;
+    
 }
