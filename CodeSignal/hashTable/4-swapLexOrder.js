6ac20ca69f79c386ce998a161347ec2bdbea0bc6
Kevin Bai <lkevin957@gmail.com>
2019-04-27 17:38:49 -0700

    Add reverse order strategy

--- CodeSignal/hashTable/4-swapLexOrder.js
+++ CodeSignal/hashTable/4-swapLexOrder.js
@@ -10,11 +10,39 @@
         // the alphabet positions of the letter should be ordered from biggest to smallest
         // dbcd > abcd because d comes after a in the alphabet
 
-// strategy
+// strategy 1
     // create a hash of the alphabet, thus allowing lexicographical comparision of letters
     // generate all possible orderings of the string
     // using an updating largest value, compare all string indices
     
-function swapLexOrder(str, pairs) {
+// strategy 2
+    // create a sorted array of all unique numbers provided in the pairs
+        // create default sorted order 1, 2, 3
+        // create a dup of reversed order 3, 2, 1
+        // make those changes, and return that as the answer
 
+function swapLexOrder(str, pairs) {
+    let normalOrder = new Array();
+    let set = new Set();
+    for (let i = 0; i < pairs.length; i++) {
+        const pair = pairs[i];
+
+        let j = pair[0];
+        if (!set.has(j)){
+            set.add(j);
+            normalOrder.push(j);
+        }
+
+        let k = pair[1];
+        if (!set.has(k)){
+            set.add(k);
+            normalOrder.push(k);
+        }
+    }
+    
+    normalOrder = normalOrder.sort();
+
+    let reverseOrder = normalOrder.reverse();
+
+    return reverseOrder;
 }
