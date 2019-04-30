4cd6ff883d14be653467af35b2b7742dab80cae9
Kevin Bai <lkevin957@gmail.com>
2019-04-29 18:56:34 -0700

    Solve meangrouping

---
+++ CodeSignal/assessments/1-meanGroups.js
@@ -0,0 +1,45 @@
+// input
+    // array of arrays a
+
+// output
+    // group the arrays a[i] by their mean values
+    // each group should have the index of the array
+    // return answer as array of arrays;
+
+
+function meanGroups(a) {
+    let averages = {};
+
+    for (let index = 0; index < a.length; index++) {
+        const subArray = a[index];
+        const mean = subArray.reduce((acc, val) => acc + val)/subArray.length;
+
+        if (averages[mean]){
+            averages[mean].push(index);
+        } else {
+            averages[mean] = [index];
+        }
+    }
+
+    return Object.values(averages).sort((a,b) => {
+        let minA = a[0];
+
+        for (let index = 0; index < a.length; index++) {
+            const number = a[index];
+            if (number < minA){
+                minA = number;
+            }
+        }
+
+        let minB = b[0];
+
+        for (let index = 0; index < b.length; index++) {
+            const number = b[index];
+            if (number < minB) {
+                minB = number;
+            }
+        }
+        
+        return minA < minB;
+    });
+}
