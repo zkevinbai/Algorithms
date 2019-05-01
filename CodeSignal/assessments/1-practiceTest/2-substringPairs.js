f98bc139c9387ddbbb5f1d47dab0d5d9020b17f7
Kevin Bai <lkevin957@gmail.com>
2019-04-30 21:28:53 -0700

    generate frequencies

--- CodeSignal/assessments/1-practiceTest/2-substringPairs.js
+++ CodeSignal/assessments/1-practiceTest/2-substringPairs.js
@@ -6,21 +6,23 @@ function mergeStrings(s1, s2) {
 
     for (let index = 0; index < s1.length; index++) {
         const s1Char = s1[index];
-        const s2Char = s2[index];
         
         if (!s1Frequency[s1Char]) {
             s1Frequency[s1Char] = 1;
         } else if (s1Frequency[s1Char]) {
             s1Frequency[s1Char] += 1;
-        }
-        
+        } 
+    }
+
+    for (let index = 0; index < s2.length; index++) {
+        const s2Char = s2[index];
+
         if (!s2Frequency[s2Char]) {
             s2Frequency[s2Char] = 1;
         } else if (s2Frequency[s2Char]) {
             s2Frequency[s2Char] += 1;
         }
     }
-
 
 
 }
