27f457482311eff93863442e0d113f7851260059
Kevin Bai <lkevin957@gmail.com>
2019-04-30 21:50:03 -0700

    Add problem description

--- CodeSignal/assessments/1-practiceTest/1-mergeStrings.js
+++ CodeSignal/assessments/1-practiceTest/1-mergeStrings.js
@@ -57,3 +57,15 @@ function mergeStrings(s1, s2) {
 
     return combined.join("");
 }
+
+
+// You are implementing your own programming language and you've decided to add support for merging strings. A typical merge function would take two strings s1 and s2, and return the lexicographically smallest result that can be obtained by placing the symbols of s2 between the symbols of s1 in such a way that maintains the relative order of the characters in each string.
+
+// For example, if s1 = "super" and s2 = "tower", the result should be merge(s1, s2) = "stouperwer".
+
+
+
+//     You'd like to make your language more unique, so for your merge function, instead of comparing the characters in the usual lexicographical order, you'll compare them based on how many times they occur in their respective strings(fewer occurrences means the character is considered smaller).If the number of occurrences are equal, then the characters should be compared in the usual way.If both number of occurences and characters are equal, you should take the characters from the first string to the result.
+
+// Given two strings s1 and s2, return the result of the special merge function you are implementing.
+
--- CodeSignal/assessments/1-practiceTest/2-substringPairs.js
+++ CodeSignal/assessments/1-practiceTest/2-substringPairs.js
@@ -17,3 +17,25 @@ function substringPairs(s, k) {
 }
 
 function diff 
+
+
+// You're creating a new programming language with some exciting new features! Any programming language can check if two strings are matching, but you'd like yours to be able to check if they're almost matching. More specifically, we'll say two strings are almost matching if they're equal in length and all of their corresponding characters are the same except for one. For example, "cat" and "bat" are almost matching, but "cat" and "dog" are not.
+
+// For the sake of efficiency, you're planning on testing the feature by using a single string and comparing its substrings. Given a string s and an integer k, your task is to find the number of pairs of substrings of s that are almost matching but differ at their kth character (0-based). It's necessary that the length of both substrings exceeds k(otherwise the strings wouldn't have a kth character).
+
+// Also note that substrings are determined by their indices, so there could potentially be multiple instances of the same word.For example, in the word "ingratiating" the substring "ing" beginning at index 0 is considered distinct from the one at index 9(and there are also two distinct "ati" substrings).
+
+//         Example
+// For s = "abacaba" and k = 1, the output should be
+// substringPairs(s, k) = 8.
+
+// Using i and j to represent the start and end indices of the first substrings, and l and m to represent the indices of the second substring, the 8 pairs are:
+
+//     ("aba", "aca") - i = 0, j = 2, l = 2, m = 4;
+// ("aba", "aca") - i = 4, j = 6, l = 2, m = 4;
+// ("aca", "aba") - i = 2, j = 4, l = 0, m = 2;
+// ("aca", "aba") - i = 2, j = 4, l = 4, m = 6;
+// ("ac", "ab") - i = 2, j = 3, l = 0, m = 1;
+// ("ac", "ab") - i = 2, j = 3, l = 4, m = 5;
+// ("ab", "ac") - i = 0, j = 1, l = 2, m = 3;
+// ("ab", "ac") - i = 4, j = 5, l = 2, m = 3.
