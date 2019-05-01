a28ebb915791814d20da8fec6c09495cd675e200
Kevin Bai <lkevin957@gmail.com>
2019-04-30 21:01:35 -0700

    add strategy

--- CodeSignal/assessments/1-practiceTest/0-alternatingSort.js
+++ CodeSignal/assessments/1-practiceTest/0-alternatingSort.js
@@ -13,6 +13,26 @@
 // a 0 1 2 3
 // b 0 3 2 1
 
+// strategy
+    // create an array of all evens
+    // create an array of all odds
+    // reverse the odds array
+    // create array b and check along the way
+
 function alternatingSort(a) {
+    let evens = [];
+    let odds = [];
+
+    // fill both arrays
+    for (let index = 0; index < a.length; index++) {
+        let number = a[index];
+        if (index % 2 === 0){
+            evens.push(number);
+        } else {
+            odds.push(number);
+        }
+    }
+
+    odds = 
 
 }
