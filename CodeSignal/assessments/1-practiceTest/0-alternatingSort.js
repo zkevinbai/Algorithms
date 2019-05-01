0945a11604b0f79fa2d19d09581544257442d7bc
Kevin Bai <lkevin957@gmail.com>
2019-04-30 20:58:44 -0700

    add alternating sort

---
+++ CodeSignal/assessments/1-practiceTest/0-alternatingSort.js
@@ -0,0 +1,18 @@
+// input
+    // array of integers a
+
+// output
+    // create new array b
+        // b[0] is equal to a[0];
+        // b[1] is equal to the last element of a;
+        // b[2] is equal to a[1];
+        // b[3] is equal to the second - last element of a;
+        // and so on.
+    // return boolean if new array b is sorted in strictly ascending order
+
+// a 0 1 2 3
+// b 0 3 2 1
+
+function alternatingSort(a) {
+
+}
