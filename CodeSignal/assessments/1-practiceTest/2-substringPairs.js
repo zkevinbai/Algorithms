4eb44beecbe96ebacd92136fa4b46ac5678700e6
Kevin Bai <lkevin957@gmail.com>
2019-04-30 21:47:29 -0700

    generate substrings

---
+++ CodeSignal/assessments/1-practiceTest/2-substringPairs.js
@@ -0,0 +1,17 @@
+function substringPairs(s, k) {
+    let subs = [];
+
+    for (let i = 0; i < s.length; i++) {
+        let current = s[i];
+        let sub = [current]
+
+        for (let j = i + 1; j < s.length; j++) {
+            let subCurrent = s[j];
+            sub.push(subCurrent);
+
+            subs.push(sub.slice(0));
+        }
+    }
+
+    return subs;
+}
