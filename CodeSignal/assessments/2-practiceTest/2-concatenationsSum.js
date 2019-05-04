1c56c2a50cdd631d747baa4b3a95bd84f5127f6a
Kevin Bai <lkevin957@gmail.com>
2019-05-03 18:51:35 -0700

    complete concatenation Sum

---
+++ CodeSignal/assessments/2-practiceTest/2-concatenationsSum.js
@@ -0,0 +1,16 @@
+function concatenationsSum(a) {
+    let allNumbers = [];
+
+    for (let i = 0; i < a.length; i++) {
+        const stringNumberI = a[i] + "";
+        for (let j = 0; j < a.length; j++) {
+            const stringNumberJ = a[j] + "";
+
+            let combined = stringNumberI + stringNumberJ;
+            combined = parseInt(combined);
+            allNumbers.push(combined);
+        }   
+    }
+
+    return allNumbers.reduce((accumulator, currentValue) => accumulator + currentValue);
+}
