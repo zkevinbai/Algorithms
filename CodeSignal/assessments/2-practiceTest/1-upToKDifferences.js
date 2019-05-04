e172b1484fa55b0d71d4b483521ee46416316283
Kevin Bai <lkevin957@gmail.com>
2019-05-03 19:08:02 -0700

    complete k differences

--- CodeSignal/assessments/2-practiceTest/1-upToKDifferences.js
+++ CodeSignal/assessments/2-practiceTest/1-upToKDifferences.js
@@ -1,23 +1,26 @@
 // select a range of strings that has k different things in it
 
 function upToKDifferences(str, k) {
-    let split = [];
+    let splitCount = 1;
 
     let array = str.split("");
 
     for (let i = 0; i < array.length; i++) {
         let slice = new Set([array[i]])
+        
         for (let j = i + 1; j < array.length; j++) {
             slice.add(array[j]);
 
             if(slice.size === k){
-                split.push(array.slice(i,j).join(""))
+                splitCount += 1;
                 i = j;
                 j = i + 1;
             }
         }
     }
 
-    return split;
+    return splitCount;
 }
 
+let str = "aabeefegeeccrr";
+let k = 3;
