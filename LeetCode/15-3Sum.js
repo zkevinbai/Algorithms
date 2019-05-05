178f70e0dd66941829c9904a8f47ee818854aff6
Kevin Bai <lkevin957@gmail.com>
2019-05-04 17:21:45 -0700

    Add triple move strategy

--- LeetCode/15-3Sum.js
+++ LeetCode/15-3Sum.js
@@ -1,39 +1,71 @@
-function iterativeSubsets(array) {
-    let res = [[]];
+function threeSum(nums) {
+    let sorted = nums.sort();
 
-    for (let i = 0; i < array.length; i++) {
-        const number = array[i];
-        const newArrs = [];
-        for (let j = 0; j < res.length; j++) {
-            newArrs.push(res[j].concat(number));
-        }
-        res = res.concat(newArrs);
-    }
+    let zeroes = [];
 
-    return res;
-}
-
-function threeSum (nums) {
-    let subSets = iterativeSubsets(nums);
-
-    let response = [];
-
-    for (let index = 0; index < subSets.length; index++) {
-        const subArray = subSets[index];
-        
-        if(subArray.length === 3 && subArray.reduce( (acc, val) => acc + val ) === 0 ){
-            response.push(subArray);
+    for (let a = 0; a < sorted.length - 2; a++) {
+        const elementA = sorted[a];
+        for (let b = a + 1; b < sorted.length - 1; b++) {
+            const elementB = sorted[b];
+            for (let c = b + 1; c < sorted.length; c++) {
+                const elementC = sorted[c];
+                if (elementA + elementB + elementC === 0) {
+                    zeroes.push([elementA, elementB, elementC]);
+                }
+            }
         }
     }
 
+    // make unique
     let object = {};
 
-    for (let index = 0; index < response.length; index++) {
-        let subArray = response[index].sort();
+    for (let index = 0; index < zeroes.length; index++) {
+        let subArray = zeroes[index].sort();
         let stringSub = subArray + "";
 
         object[stringSub] = subArray;
     }
-    
+
     return Object.values(object).sort();
 };
+
+
+// function iterativeSubsets(array) {
+//     let res = [[]];
+
+//     for (let i = 0; i < array.length; i++) {
+//         const number = array[i];
+//         const newArrs = [];
+//         for (let j = 0; j < res.length; j++) {
+//             newArrs.push(res[j].concat(number));
+//         }
+//         res = res.concat(newArrs);
+//     }
+
+//     return res;
+// }
+
+// function threeSum (nums) {
+//     let subSets = iterativeSubsets(nums);
+
+//     let response = [];
+
+//     for (let index = 0; index < subSets.length; index++) {
+//         const subArray = subSets[index];
+        
+//         if(subArray.length === 3 && subArray.reduce( (acc, val) => acc + val ) === 0 ){
+//             response.push(subArray);
+//         }
+//     }
+
+//     let object = {};
+
+//     for (let index = 0; index < response.length; index++) {
+//         let subArray = response[index].sort();
+//         let stringSub = subArray + "";
+
+//         object[stringSub] = subArray;
+//     }
+    
+//     return Object.values(object).sort();
+// };
