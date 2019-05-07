5533c76cc6d2dae93fbbb07cc026edeff9d1e445
Kevin Bai <lkevin957@gmail.com>
2019-05-06 19:27:41 -0700

    add bfs

--- CodeSignal/tree/0-hasPathWithGivenSum.js
+++ CodeSignal/tree/0-hasPathWithGivenSum.js
@@ -7,5 +7,26 @@
 // }
 
 function hasPathWithGivenSum(t, s) {
+    let queue = [t];
+    let paths = [];
 
+    while(queue.length){
+        let shift = queue.shift();
+
+        let newArray = [];
+        
+        paths.forEach( path => newArray.push(path.push(shift.value)) )
+
+        paths = newArray;
+
+        if(shift.left){
+            queue.push(shift.left);
+        }
+
+        if(shift.right){
+            queue.push(shift.right);
+        }
+    }
+
+    return paths;
 }
