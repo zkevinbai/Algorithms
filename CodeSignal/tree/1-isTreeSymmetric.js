bb3422eb749f8d2d8255b09574e8ee2e62791ef3
Kevin Bai <lkevin957@gmail.com>
2019-05-06 20:18:25 -0700

    Achieve 5/7 solution

--- CodeSignal/tree/1-isTreeSymmetric.js
+++ CodeSignal/tree/1-isTreeSymmetric.js
@@ -5,6 +5,29 @@
 //   this.left = null;
 //   this.right = null;
 // }
-function isTreeSymmetric(t) {
 
+function isTreeSymmetric(t) {
+    if (!t) return true;
+    let leftTree = true;
+    let rightTree = true;
+
+    if (t.left) leftTree = treeTraversal(t.left);
+    if (t.right) rightTree = treeTraversal(t.right);
+
+    return leftTree + "" === rightTree + "";
+}
+
+function treeTraversal(tree) {
+    let array = [];
+    let queue = [tree];
+
+    while (queue.length) {
+        let shift = queue.shift();
+        array.push(shift.value);
+
+        if (shift.left) queue.push(shift.left);
+        if (shift.right) queue.push(shift.right);
+    }
+
+    return array;
 }
