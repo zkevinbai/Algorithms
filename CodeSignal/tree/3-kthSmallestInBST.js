d05fe9790664f7a4e41433a526abed421a0dbddc
Kevin Bai <lkevin957@gmail.com>
2019-05-07 11:38:55 -0700

    Achieve 5/6 solution with BFS and sorting

---
+++ CodeSignal/tree/3-kthSmallestInBST.js
@@ -0,0 +1,36 @@
+//
+// Definition for binary tree:
+// function Tree(x) {
+//   this.value = x;
+//   this.left = null;
+//   this.right = null;
+// }
+
+// input
+
+// output
+
+// strategy
+    // BFS traversal
+    // sort traversal
+    // find the nth largest
+
+function kthSmallestInBST(t, k) {
+    let values = [];
+
+    let queue = [t];
+
+    while(queue.length){
+        let shift = queue.shift();
+        values.push(shift.value);
+
+        if(shift.left){
+            queue.push(shift.left)
+        }
+        if(shift.right){
+            queue.push(shift.right)
+        }
+    }
+
+    return values.sort()[k-1];
+}
