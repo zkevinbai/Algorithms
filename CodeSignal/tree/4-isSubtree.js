dc21553259d13e3a6798913fc3a7c795d663c65a
Kevin Bai <lkevin957@gmail.com>
2019-05-07 14:33:12 -0700

    Add isSubtree

---
+++ CodeSignal/tree/4-isSubtree.js
@@ -0,0 +1,20 @@
+//
+// Definition for binary tree:
+// function Tree(x) {
+//   this.value = x;
+//   this.left = null;
+//   this.right = null;
+// }
+
+// strategy
+    // bfs the big tree
+        // when a node's value is the same as the sub tree's root value
+            // run a double bfs traversal
+            // if full traversal, return true
+
+    // return false at the end
+
+
+function isSubtree(t1, t2) {
+
+}
