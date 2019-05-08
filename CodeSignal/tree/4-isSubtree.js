eaa9c8aefb680fb5c5b2c2bdc137ef8b142b77aa
Kevin Bai <lkevin957@gmail.com>
2019-05-07 20:12:54 -0700

    Add comments

--- CodeSignal/tree/4-isSubtree.js
+++ CodeSignal/tree/4-isSubtree.js
@@ -6,6 +6,34 @@
 //   this.right = null;
 // }
 
+// Phase 2 - someone else's code
+
+function isSubtree(t1, t2) {
+    //if (!t1 && !t2) return true
+    if (!t2) return true
+    if (!t1) return false
+
+    //t2 is a sub of t1 if
+    //t1.left is t2 
+    //or
+    //t1.right is t2
+    if (equal(t1.left, t2) || equal(t1.right, t2)) return true
+    else return isSubtree(t1.left, t2) || isSubtree(t1.right, t2)
+}
+
+
+function equal(t1, t2) {
+    if (!t1 && !t2) return true
+    if (!t1 || !t2) return false
+    if (t1.value !== t2.value) return false
+    return equal(t1.left, t2.left) && equal(t1.right, t2.right)
+}
+
+
+
+
+// Phase 1 - my solution 13/14 timed out
+
 // strategy
     // bfs the big tree
         // when a node's value is the same as the sub tree's root value
@@ -13,7 +41,6 @@
             // if node and sub root are the same, return true
 
     // return false at the end
-
 
 function isSubtree(t1, t2) {
     if (!t1 && !!t2) return false;
