c18627bb6b595cbbd0a53670f92a709682d7c87b
Kevin Bai <lkevin957@gmail.com>
2019-05-09 09:24:51 -0700

    Add find substrings

--- CodeSignal/tree/6-findSubstrings.js
+++ CodeSignal/tree/6-findSubstrings.js
@@ -1,3 +1,15 @@
+// input
+    // array of words
+    // array of parts
+// output
+    // array of words, with the first/ longest part surrounded by brackets
+
+// strategy
+    // for each word, iterate through the parts, if it includes
+        // find first index of
+        // slice into 3
+        // add brackets
+
 function findSubstrings(words, parts) {
 
 }
