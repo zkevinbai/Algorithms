be49366d355deb6552b319efa9bd92447e2e9c5a
Kevin Bai <lkevin957@gmail.com>
2019-05-09 11:04:23 -0700

    save an if statement

--- CodeSignal/tree/6-findSubstrings.js
+++ CodeSignal/tree/6-findSubstrings.js
@@ -4,6 +4,53 @@
 // output
 // array of words, with the first/ longest part surrounded by brackets
 
+
+
+function findSubstrings(words, parts) {
+    let response = [];
+
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+
+        let largestFirstPart = "";
+        for (let j = 0; j < parts.length; j++) {
+            let part = parts[j];
+
+            if (word.includes(part)){
+                if (part.length === largestFirstPart.length && word.indexOf(part) < word.indexOf(largestFirstPart)) {
+                    largestFirstPart = part;
+                } else if (part.length > largestFirstPart.length) {
+                    largestFirstPart = part;
+                }
+            }            
+        }
+
+        if (largestFirstPart.length) {
+            let startingIndex = word.indexOf(largestFirstPart);
+            let endingIndex = startingIndex + largestFirstPart.length;
+
+            let wordArr = word.split("");
+
+            let left = wordArr.slice(0, startingIndex);
+            let mid = wordArr.slice(startingIndex, endingIndex);
+            let right = wordArr.slice(endingIndex);
+
+            left.push("[");
+            mid.push("]");
+
+            let newWord = left.join("") + mid.join("") + right.join("");
+            response.push(newWord);
+        } else {
+            response.push(word);
+        }
+    }
+
+    return response;
+}
+
+
+
+// phase 1 14/16
 // strategy
 // for each word, iterate through the parts, if it includes
 // find first index of
