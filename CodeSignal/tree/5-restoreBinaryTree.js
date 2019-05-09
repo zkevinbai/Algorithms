915fe1dc109461ad4f79b0f77a1da8e55e5c66f3
Kevin Bai <lkevin957@gmail.com>
2019-05-08 20:17:21 -0700

    Add restoreBinaryTree

---
+++ CodeSignal/tree/5-restoreBinaryTree.js
@@ -0,0 +1,10 @@
+//
+// Definition for binary tree:
+// function Tree(x) {
+//   this.value = x;
+//   this.left = null;
+//   this.right = null;
+// }
+function restoreBinaryTree(inorder, preorder) {
+
+}
