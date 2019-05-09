469e4e62c206c541e1e42a5bc7a3837fa9274ca9
Kevin Bai <lkevin957@gmail.com>
2019-05-08 20:22:39 -0700

    Add restoration logic

--- CodeSignal/tree/5-restoreBinaryTree.js
+++ CodeSignal/tree/5-restoreBinaryTree.js
@@ -5,6 +5,15 @@
 //   this.left = null;
 //   this.right = null;
 // }
-function restoreBinaryTree(inorder, preorder) {
 
+function restoreBinaryTree(inorder, preorder) {
+    let shift = preorder.shift();
+    let root = Tree(shift);
+
+    let inorderRootIndex = inorder.indexOf(shift);
+
+    root.left = restoreBinaryTree(inorder.slice(0, inorderRootIndex), preorder)
+    root.left = restoreBinaryTree(inorder.slice(inorderRootIndex, inorder.length), preorder)
+
+    return root;
 }
