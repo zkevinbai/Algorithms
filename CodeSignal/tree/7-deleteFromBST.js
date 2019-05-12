febd1033d48be6b8107aabd74ded331d6a9ea308
Kevin Bai <lkevin957@gmail.com>
2019-05-11 18:47:01 -0700

    Add BFS logic

--- CodeSignal/tree/7-deleteFromBST.js
+++ CodeSignal/tree/7-deleteFromBST.js
@@ -5,6 +5,26 @@
 //   this.left = null;
 //   this.right = null;
 // }
-function deleteFromBST(t, queries) {
 
+function deleteFromBST(t, queries) {
+    for (let i = 0; i < queries.length; i++) {
+        let query = queries[i];
+
+        let foundNode;
+        let queue = [t];
+
+        while(queue.length){
+            let shift = queue.shift();
+            if(shift.value === query){
+                foundNode = shift;
+            }
+        }
+        
+    }
 }
+
+// search BST
+
+// find the element
+
+// run deletion algorithm
