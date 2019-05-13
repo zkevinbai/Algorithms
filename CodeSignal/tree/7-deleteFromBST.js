bb86ee76c6923d8cbff34f19b7676b40bef51fb2
Kevin Bai <lkevin957@gmail.com>
2019-05-12 19:30:11 -0700

    Add logic to use deletion code

--- CodeSignal/tree/7-deleteFromBST.js
+++ CodeSignal/tree/7-deleteFromBST.js
@@ -6,6 +6,108 @@
 //   this.right = null;
 // }
 
+// strategy 2 - someone else's code
+
+function deleteFromBST(t, queries){
+
+    for (let index = 0; index < queries.length; index++) {
+        const query = queries[index];
+        
+        t = deleteNode(t, query);
+    }
+
+    return t;
+}
+
+function deleteNode(root, key) {
+    // Find node to delete.
+    if (root !== null) {
+        let current = root;
+        let stack = [];
+
+        while (current) {
+            stack.push(current);
+
+            if (current.value === key) {
+                // Found the node to delete.
+                stack.pop();
+                let parent = stack.pop();
+
+                if (!current.left && !current.right) {
+                    // No children, just remove the node.
+                    if (parent && parent.left && parent.left.value === current.value) {
+                        parent.left = null;
+                    }
+                    else if (parent) {
+                        parent.right = null;
+                    }
+                    else {
+                        // No parent, this must be the root node.
+                        root = [];
+                    }
+                }
+                else if (current.left && !current.right) {
+                    // One left child node.
+                    if (parent && parent.left && parent.left.value === current.value) {
+                        parent.left = current.left;
+                    }
+                    else if (parent) {
+                        parent.right = current.left;
+                    }
+                    else {
+                        // No parent, this must be the root node.
+                        root = current.left;
+                    }
+                }
+                else if (current.right && !current.left) {
+                    // One right child node.
+                    if (parent && parent.left && parent.left.value === current.value) {
+                        parent.left = current.right;
+                    }
+                    else if (parent) {
+                        parent.right = current.right;
+                    }
+                    else {
+                        // No parent, this must be the root node.
+                        root = current.right;
+                    }
+                }
+                else {
+                    // Node has 2 children.
+                    // First, find the minimum element in the right subtree of the node to be removed.
+                    let minNode = current.right;
+                    while (minNode) {
+                        if (minNode.left) {
+                            minNode = minNode.left;
+                        }
+                        else {
+                            // We're at the bottom of the subtree.
+                            break;
+                        }
+                    }
+
+                    // Delete minNode.
+                    current = deleteNode(current, minNode.value);
+
+                    // Replace value.
+                    current.value = minNode.value;
+                }
+                break;
+            }
+            else if (key < current.value) {
+                current = current.left;
+            }
+            else if (key > current.value) {
+                current = current.right;
+            }
+        }
+    }
+
+    return root;
+};
+
+// strategy 1
+
 // search BST
 
 // find the element
