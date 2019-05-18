868644d5d1095db38efd1daad52904512a9500e6
Kevin Bai <lkevin957@gmail.com>
2019-05-17 20:21:11 -0700

    Complete kth Largest

--- CodeSignal/heap/0-kthLargest.js
+++ CodeSignal/heap/0-kthLargest.js
@@ -1,7 +1,7 @@
 
 // strategy 1 - sorting and indexing O(n)
 function kthLargestElement(nums, k) {
-    nums = nums.sort((a, b) => a > b);
+    nums = nums.sort((a, b) => a - b);
 
     let kleIndex = nums.length - k;
 
