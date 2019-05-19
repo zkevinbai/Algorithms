1c7dd71c818b0cb0cbc0626f5b5c99c3497f880d
Kevin Bai <lkevin957@gmail.com>
2019-05-18 19:00:41 -0700

    add 1 to represent depth

--- LeetCode/104-maxDepthofBST.js
+++ LeetCode/104-maxDepthofBST.js
@@ -1,13 +1,13 @@
 function maxDepth(root) {
-    if(!root) return 0;
-    if(!root.left && !root.right) return 1;
+    if (!root) return 0;
+    if (!root.left && !root.right) return 1;
 
     let leftTraversal = 0;
     let rightTraversal = 0;
 
-    if(root.left) leftTraversal += maxDepth(root.left);
-    if(root.right) rightTraversal += maxDepth(root.right);
+    if (root.left) leftTraversal += maxDepth(root.left);
+    if (root.right) rightTraversal += maxDepth(root.right);
 
-    if(leftTraversal > rightTraversal) return leftTraversal;
-    return rightTraversal;
+    if (leftTraversal > rightTraversal) return leftTraversal + 1;
+    return rightTraversal + 1;
 };
