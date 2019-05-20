5b0932159079b9a0d93316997780ed28e05fb7cd
Kevin Bai <lkevin957@gmail.com>
2019-05-19 23:25:55 -0700

    Account for base case

---
+++ LeetCode/136-SingleNumber.js
@@ -0,0 +1,21 @@
+var singleNumber = function (nums) {
+    if(nums.length === 1) return nums[0];
+
+    nums = nums.sort((a, b) => a - b);
+
+    for (let index = 0; index < nums.length; index++) {
+        let number = nums[index];
+        let prev;
+        let next;
+
+        if(index === 0){
+            next = nums[index + 1];
+            if(number !== next) return number;
+        } else if(index === nums.length - 1){
+
+        } else {
+
+        }
+        
+    }
+};
