8444490c63b4b7a2c0a61d04f7a91b86df264d7a
Kevin Bai <lkevin957@gmail.com>
2019-05-21 08:38:32 -0700

    Add revese logic

--- LeetCode/206-ReverseLinkedList.js
+++ LeetCode/206-ReverseLinkedList.js
@@ -1,3 +1,14 @@
 const reverseList = function (head) {
+    let prev = null;
+    let curr = head;
+    let next = null;
 
+    while(curr){
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
 };
