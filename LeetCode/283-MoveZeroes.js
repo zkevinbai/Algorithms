fe0e4c16a6789772ec077d8e06fb941f2eb2c08b
Kevin Bai <lkevin957@gmail.com>
2019-05-21 09:41:45 -0700

    Solve move zeroes

--- LeetCode/283-MoveZeroes.js
+++ LeetCode/283-MoveZeroes.js
@@ -1,11 +1,22 @@
-const moveZeroes = function (nums) {
-    nums.sort((a, b) => a - b);
-
-    while (nums[0] === 0) {
-        let shift = nums.shift();
-
-        nums.push(shift)
+// Working solution
+var moveZeroes = function (nums) {
+    for (let index = nums.length - 1; index >= 0; index--) {
+        if (nums[index] === 0) {
+            nums.splice(index, 1);
+            nums.push(0);
+        }
     }
-
-    return nums
 };
+
+// Working solution (if stable sort, so Chrome not Node)
+
+// const moveZeroes = function (nums) {
+//     nums.sort(function (a, b) {
+//         if (a == 0 && b != 0)
+//             return 1;
+//         else if (b == 0 && a != 0)
+//             return -1;
+//         else
+//             return 0;
+//     });
+// };
