185a0774318a46953d8acbc8d8759aee32d907af
Kevin Bai <lkevin957@gmail.com>
2019-05-21 19:01:23 -0700

    Add notes

--- LeetCode/237-DeleteNodeLinkedList.js
+++ LeetCode/237-DeleteNodeLinkedList.js
@@ -1,3 +1,6 @@
+// this question is jank
+// we are not actually deleting a linked list node
+//  we are just shifting everything down by one
 
 var deleteNode = function (node) {
     node.val = node.next.val;
