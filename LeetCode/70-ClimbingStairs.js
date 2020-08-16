a823352889e831b7ed889b9d035aa3bd67709c1b
Kevin Bai <lkevin957@gmail.com>
2020-08-15 22:16:55 -0700

    simplify intitial condition

--- LeetCode/70-ClimbingStairs.js
+++ LeetCode/70-ClimbingStairs.js
@@ -55,15 +55,23 @@ Constraints:
 //     return fibonacci(n + 1);
 // };
 
+// const climbStairs = function (n) {
+//     if (n === 1) {
+//         return 1;
+//     }
+//     if (n === 2) {
+//         return 2;
+//     }
+//     if (n === 3) {
+//         return 3;
+//     }
+
+//     return climbStairs(n - 1) + climbStairs(n - 2);
+// };
+
 const climbStairs = function (n) {
-    if (n === 1) {
-        return 1;
-    }
-    if (n === 2) {
-        return 2;
-    }
-    if (n === 3) {
-        return 3;
+    if (n < 4) {
+        return n;
     }
 
     return climbStairs(n - 1) + climbStairs(n - 2);
