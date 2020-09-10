a863e08a6b7f48c6a26d05dc37d91c466c740a4b
Kevin Bai <lkevin957@gmail.com>
2020-09-10 14:40:55 -0700

    Add example

--- LeetCode/206-ReverseLinkedList.js
+++ LeetCode/206-ReverseLinkedList.js
@@ -12,3 +12,22 @@ const reverseList = function (head) {
 
     return prev;
 };
+
+// Linked list
+
+// const nodeOne = {
+//     next: nodeTwo,
+//     value: 1
+// }
+
+// const nodeTwo = {
+//     next: nodeThree,
+//     value: 2
+// }
+
+// const nodeThree = {
+//     next: null,
+//     value: 2
+// }
+
+
