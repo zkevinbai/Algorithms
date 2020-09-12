5ef9f36aec92c08b6212a3b393923fea02fd54c9
Kevin Bai <lkevin957@gmail.com>
2020-09-12 16:15:38 -0700

    Add notes

--- LeetCode/20-ValidParenthesis.js
+++ LeetCode/20-ValidParenthesis.js
@@ -5,11 +5,22 @@ An input string is valid if:
 
 Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
-
-
-
 */
 
-const isValid = (s) => {
+// this is a stack problem
+// add values to stack, and pop them off as the pairs show up
+// if the stack is empty at the end of the iteration, the string is valid
+
+const isValid = (string) => {
+    const array = string.split("");
+
+    const stack = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const value = array[i];
+
+        
+    }
+
 
 };
