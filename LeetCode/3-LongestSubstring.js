d8dcbacddd2a5765c6404409e744e82582205bdd
Kevin Bai <lkevin957@gmail.com>
2020-09-13 13:12:36 -0700

    Simplify to use substring.length

--- LeetCode/3-LongestSubstring.js
+++ LeetCode/3-LongestSubstring.js
@@ -29,24 +29,23 @@ Constraints:
 s consists of English letters, digits, symbols and spaces.
 */
 
+// dvdf edge case
+
 const lengthOfLongestSubstring = (string) => {
     const array = string.split("");
     let subString = [];
     let maxLength = 0;
-    let currentLength = 0;
 
     for (let i = 0; i < array.length; i++) {
         const character = array[i];
         if (subString.includes(character)) {
             subString = [character];
-            currentLength = 1;
         } else {
             subString.push(character);
-            currentLength +=1;
         }
 
-        if (currentLength > maxLength) {
-            maxLength = currentLength;
+        if (subString.length > maxLength) {
+            maxLength = subString.length;
         }
     }
 
