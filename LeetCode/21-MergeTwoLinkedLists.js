5c5cf0b334065f787c729a2f861c47a9accfc36c
Kevin Bai <lkevin957@gmail.com>
2020-09-12 17:12:56 -0700

    Add 21 merge 2 linked lists

---
+++ LeetCode/21-MergeTwoLinkedLists.js
@@ -0,0 +1,14 @@
+/*
+Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+*/
