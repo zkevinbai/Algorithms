b14048b64354a318cc32995beba954d5fe508aab
Kevin Bai <lkevin957@gmail.com>
2020-09-13 14:44:44 -0700

    Add is palindrome function

---
+++ LeetCode/5-LongestPalindromicSubstring.js
@@ -0,0 +1,44 @@
+/*
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: "cbbd"
+Output: "bb"
+*/
+
+const longestPalindrome = (string) => {
+
+};
+
+// get to half point, cut the array
+// see if both halves are equal
+
+const isPalindrome = (array) => {
+    const arrayLength = array.length;
+    const isOdd = (arrayLength % 2);
+    const pivot = arrayLength / 2;
+
+    let leftHalf = array.slice(0, pivot);
+    let rightHalf = array.slice(pivot, arrayLength + 1);
+
+    if (isOdd) {
+        rightHalf = array.slice(pivot + 1, arrayLength + 1);
+    }
+
+    for (let i = 0; i < leftHalf.length; i++) {
+        const leftVal = leftHalf[i];
+        const rightVal = rightHalf[i];
+
+        if (leftVal !== rightval) {
+            return false;
+        }
+    }
+
+    return true;
+}
