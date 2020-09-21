e1bf9f123107f4a0ad3c901144795db0d000bdea
Kevin Bai <lkevin957@gmail.com>
2020-09-20 18:41:51 -0700

    Add 160 intersection of linked lists

---
+++ LeetCode/160-IntersectionOfLinkedLists.js
@@ -0,0 +1,16 @@
+/*
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Reference of the node with value = 8
+
+Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Reference of the node with value = 2
+
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: null
+*/
+
+const getIntersectionNode = function (headA, headB) {
+
+};
