069eb0cc6fe3de307cab6f2e963e0353796391c9
Kevin Bai <lkevin957@gmail.com>
2020-10-03 13:03:49 -0700

    Achieve base solution

---
+++ LeetCode/17-LetterCombinationsOfNumber.js
@@ -0,0 +1,47 @@
+/* 
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example:
+
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+*/
+const phone = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+}
+
+const letterCombinations = (digits) => {
+    const numbers = digits.split('');
+    // pre populate with first letter
+    let combinations = phone[numbers.shift()];
+
+    for (let i = 0; i < numbers.length; i++) {
+        const number = numbers[i];
+        const letters = phone[number];
+        let newCombinations = [];
+
+        console.log(combinations)
+
+        letters.forEach(letter => {
+            newCombinations = newCombinations.concat(
+                combinations.map(value => value + letter)
+            );
+        });
+
+        combinations = newCombinations;
+    }
+
+    return combinations;
+};
