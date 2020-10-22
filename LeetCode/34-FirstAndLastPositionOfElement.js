b5c133ca83635a56ce8835648234fb2cc2b086df
Kevin Bai <lkevin957@gmail.com>
2020-10-21 22:29:11 -0700

    Add ologn solution

--- LeetCode/34-FirstAndLastPositionOfElement.js
+++ LeetCode/34-FirstAndLastPositionOfElement.js
@@ -38,6 +38,30 @@ nums is a non-decreasing array.
 */
 
 //O(log(n))
+const binarySearch = (array, target) => {
+    if (!array.length) return -1;
+    if (array.length === 1 && array[0] !== target) return -1;
+
+    const pivotIndex = Math.floor(array.length / 2);
+    const pivotVal = array[pivotIndex];
+
+    const leftArray = array.slice(0, pivotIndex);
+    const rightArray = array.slice(pivotIndex, array.length);
+
+    if (pivotVal === target) {
+        return pivotIndex;
+    } else if (pivotVal < target) {
+        const rightSearch = binarySearch(rightArray, target);
+        if (rightSearch > 0) {
+            return pivotIndex + rightSearch;
+        } else {
+            return -1;
+        }
+    } else if (pivotVal > target) {
+        return binarySearch(leftArray, target);
+    }
+}
+
 const searchRange = (array, target) => {
     if (array.length === 0) {
         return [-1, -1];
@@ -47,19 +71,21 @@ const searchRange = (array, target) => {
         return [-1, -1];
     }
 
-    let leftPointer = 0;
-    let rightPointer = array.length - 1;
+    const targetPivot = binarySearch(array, target);
 
-    while (leftPointer !== target && rightPointer !== target && leftPointer !== rightPointer) {
-        leftPointer += 1;
-    }
-
-    while (array[rightPointer] !== target && rightPointer > 0) {
-        rightPointer -= 1;
-    }
-
-    if (leftPointer === array.length - 1 && rightPointer === 0 && array[leftPointer] !== target) {
+    if (targetPivot === -1) {
         return [-1, -1];
+    }
+
+    let leftPointer = targetPivot;
+    let rightPointer = targetPivot;
+
+    while (array[leftPointer - 1] === target) {
+        leftPointer -= 1;
+    }
+
+    while (array[rightPointer - 1] === target) {
+        rightPointer -= 1;
     }
 
     return [leftPointer, rightPointer];
