7568fe166458ae709d4156ac5a9bd69f4a372d24
Kevin Bai <lkevin957@gmail.com>
2020-10-21 21:58:00 -0700

    Add 34 first and last position

---
+++ LeetCode/34-FirstAndLastPositionOfElement.js
@@ -0,0 +1,40 @@
+/* 
+Find First and Last Position of Element in Sorted Array
+Medium
+
+4251
+
+168
+
+Add to List
+
+Share
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+Follow up: Could you write an algorithm with O(log n) runtime complexity?
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+*/
+
+//O(n)
