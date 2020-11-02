5f20d761536c9aac84ec979df2b9ba78e0055947
Kevin Bai <lkevin957@gmail.com>
2020-11-01 18:20:34 -0800

    Add 46 permutations

---
+++ LeetCode/46-Permutations.js
@@ -0,0 +1,18 @@
+/*
+Given a collection of distinct integers, return all possible permutations.
+
+permutations refer to all unique orderings 
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+*/
