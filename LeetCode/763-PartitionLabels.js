bbf91f0cb018e582087d6e2fe807e0a57982dd85
Kevin Bai <lkevin957@gmail.com>
2021-03-07 23:32:39 -0800

    Add v2

--- LeetCode/763-PartitionLabels.js
+++ LeetCode/763-PartitionLabels.js
@@ -24,7 +24,86 @@ first letter start and stop
 second letter start and stop
 and so on
 */
+// v2 - build backlog of end indices, and traverse accordingly
+var partitionLabels = function (String) {
+    if (!S) return [];
 
+    const rightmostIndices = {};
+
+    for (let i = 0; i < String.length; i++) {
+        rightmostIndices[String[i]] = i;
+    }
+
+    let rightIndex = rightmostIndices[String[0]];
+    let leftIndex = 0;
+    const partitionLengths = [];
+
+    for (let i = 0; i < String.length; i++) {
+        if (i === rightmostIndex) {
+            partitionLengths.push(i - leftIndex + 1);
+            leftIndex = i + 1;
+            rightIndex = rightmostIndices[String[i + 1]];
+        } else {
+            rightIndex = Math.max(rightIndex, rightmostIndices[String[i]]);
+        }
+    }
+
+    return partitionLengths;
+};
+
+
+const partitionLabels = (string) => {
+    const letterArray = string.split("");
+
+    const visitedLetters = {};
+
+    let pointerStart = null; // storage one
+    let pointerEnd = null;
+
+    let currentPointerStart = null;
+    let currentPointerEnd = null;
+    let currentLetter = null;
+
+    const partitions = [];
+
+    for (currentPointerStart = 0; currentPointerStart < letterArray.length; currentPointerStart++) {
+
+        currentLetter = letterArray[currentPointerStart];
+
+        if (currentPointerStart === pointerEnd) {
+            const partition = pointerEnd - pointerStart + 1;
+            partitions.push(partition);
+            pointerStart = pointerEnd + 1;
+            continue;
+        } else if (visitedLetters[currentLetter]) {
+            continue;
+        }
+
+        for (let index = currentPointerStart; index < letterArray.length; index++) {
+            const letter = letterArray[index];
+
+            if (letter === currentLetter) {
+                currentPointerEnd = index;
+            }
+        }
+
+        visitedLetters[currentLetter] = true
+
+        if (currentPointerEnd > pointerEnd || currentPointerEnd === pointerStart) {
+            pointerEnd = currentPointerEnd;
+        }
+
+        if (currentPointerStart === pointerEnd) {
+            const partition = pointerEnd - pointerStart + 1;
+            partitions.push(partition);
+            pointerStart = pointerEnd + 1;
+        }
+    }
+
+    return partitions
+};
+
+// v1
 // abab
 // 3
 // 
