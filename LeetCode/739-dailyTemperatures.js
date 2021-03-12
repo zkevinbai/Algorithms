3f709ad8f016dce10adbc2d9e3a8e3c210406518
Kevin Bai <lkevin957@gmail.com>
2021-03-11 19:29:59 -0800

    Add fast stack solution

--- LeetCode/739-dailyTemperatures.js
+++ LeetCode/739-dailyTemperatures.js
@@ -5,19 +5,37 @@ For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73]
 
 Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
 */
+const dailyTemperatures_stack = function (temperature) {
+    const result = Array(temperature.length).fill(0); // prefill results
+    const stack = [];
+
+    for (let i = 0; i < temperature.length; i++) {
+        let top = stack[stack.length - 1];
+
+        while (stack.length && temperature[i] > temperature[top]) { // stack exists, current temperature > top of stack temperature
+            const topIndex = stack.pop();
+
+            result[topIndex] = i - topIndex; // update topIndex day's days until hot
+
+            top = stack[stack.length - 1]; // reset top
+        }
+        stack.push(i);
+    }
+    return result;
+};
 
 // 2 pointer O(n), 3x slower than n^2
-const dailyTemperatures2Pointer = (temperatures) => {
+const dailyTemperatures_2Pointer = (temperatures) => {
     let slow = 0; fast = 1; res = []; count = 0;
 
-    while (slow < temperatureArray.length - 1) {
+    while (slow < temperatures.length - 1) {
         count++;
         if (temperatures[fast] > temperatures[slow]) {
             res.push(count);
             count = 0;
             slow++
             fast = slow + 1;
-        } else if (fast === temperatureArray.length) {
+        } else if (fast === temperatures.length) {
             res.push(0);
             count = 0;
             slow++
