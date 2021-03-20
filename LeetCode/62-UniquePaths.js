40bbb73d0bba4a372361b504a95a937317bf1210
Kevin Bai <lkevin957@gmail.com>
2021-03-20 12:23:34 -0700

    62 unique paths

---
+++ LeetCode/62-UniquePaths.js
@@ -0,0 +1,89 @@
+/*
+62. Unique Paths
+Medium
+
+4746
+
+240
+
+Add to List
+
+Share
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Example 1:
+
+
+Input: m = 3, n = 7
+Output: 28
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+Example 3:
+
+Input: m = 7, n = 3
+Output: 28
+Example 4:
+
+Input: m = 3, n = 3
+Output: 6
+
+
+Constraints:
+
+1 <= m, n <= 100
+It's guaranteed that the answer will be less than or equal to 2 * 109.
+*/
+
+/*
+solution
+m = height of box
+n = width of box
+
+starting position is always (0, 0)
+DFS to get all possible paths
+
+increment path count each time there is a solution;
+*/
+const uniquePaths = (m, n) => {
+    let stack = [[0, 0]];
+
+    let paths = 0;
+
+    while (stack.length) {
+        const position = stack.shift();
+        const x = position[0];
+        const y = position[1];
+
+        let newPosition;
+
+        if (x === n - 1 && y === m - 1) {
+            paths += 1;
+        } else {
+
+            if (x < n - 1) {
+                newPosition = [x + 1, y];
+                stack.unshift(newPosition);
+            }
+
+            if (y < m - 1) {
+                newPosition = [x, y + 1];
+                stack.unshift(newPosition);
+            }
+
+        }
+
+    }
+
+    return paths;
+}
