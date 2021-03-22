91730100495b1ba9b7fa0ea30aa221a579be78f1
Kevin Bai <lkevin957@gmail.com>
2021-03-22 16:26:15 -0700

    Update 2

--- Interviews/22March2021_microsoftIQ.js
+++ Interviews/22March2021_microsoftIQ.js
@@ -48,7 +48,79 @@ function solution(S) {
     return Math.floor(Math.min(bCount, aCount, lCount, oCount, nCount));
 }
 
+// let example = 'QAWABAWONL';
 
-let example = 'QAWABAWONL';
+// console.log(solution(example));
+// 1 hour left
 
-console.log(solution(example));
+/* 2
+
+build a react component, a table with pagination
+
+you can get id, first name, and last name frm the url provided using the fetch api
+the endpoint accepts the query parameter "page"
+
+steps
+    1 get data
+    2 use data
+
+*/
+
+
+import React, { useState } from 'react';
+
+const USERS_URL = 'https://example.com/api/users';
+
+export default function Table() {
+
+    // const [loading, setLoading] = useState(false);
+
+    const getData = () => {
+        // setLoading(true);
+        fetch(USERS_URL + '?page=0')
+            .then(
+                function (response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' +
+                            response.status);
+                        return;
+                    }
+
+                    // Examine the text in the response
+                    response.json().then(function (data) {
+                        console.log(data);
+                    });
+                }
+            )
+            .catch(function () {
+                console.log("oh no")
+            });
+    }
+
+    getData();
+    // console.log(data);
+
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    //  render elements in tbody
+        </tbody>
+            </table>
+            <section className="pagination">
+                <button disabled={false} className="first-page-btn">first</button>
+                <button disabled={false} className="previous-page-btn">previous</button>
+                <button disabled={false} className="next-page-btn">next</button>
+                <button disabled={false} className="last-page-btn">last</button>
+            </section>
+        </div>
+    );
+};
+
