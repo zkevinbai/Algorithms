3743ac0e1ca3428f7e32363c6e26b646c402010c
Kevin Bai <lkevin957@gmail.com>
2021-03-29 10:32:25 -0700

    Add google

---
+++ Interviews/29March2021_google.js
@@ -0,0 +1,15 @@
+/*
+
+function to handle async calls to server
+
+have a list of urls from most to least reliable
+
+async / await syntax
+
+what if you want the best data
+
+
+part 2
+
+what if you want the fastest data
+*/
