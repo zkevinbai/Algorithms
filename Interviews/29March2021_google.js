7679754e33cd6b6d2d40807f0e774f31fcf7e495
Kevin Bai <lkevin957@gmail.com>
2021-04-01 10:01:27 -0700

    Solve google

--- Interviews/29March2021_google.js
+++ Interviews/29March2021_google.js
@@ -1,5 +1,6 @@
 /*
 
+part 1
 function to handle async calls to server
 
 have a list of urls from most to least reliable
@@ -8,8 +9,33 @@ async / await syntax
 
 what if you want the best data
 
+input = [ url1, url2, url3 ];
+*/
+
+const getBestData = async (urls) => {
+
+    for (let i = 0; i < urls.length; i++) {
+        const url = urls[i];
+
+        const data = await get(url);
+
+        if (data.status !== '404') {
+            return data;
+        }
+    }
+
+    // assume at least one will return
+}
+
+/*
 
 part 2
 
 what if you want the fastest data
 */
+
+const getFastestData = async (urls) => {
+    Promise.race(urls)
+        .then((result) => result)
+        .catch((error) => error)
+}
