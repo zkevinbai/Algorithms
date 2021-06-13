750e68ed87ee9fe2b8392699e4a195841dd2f2ce
Kevin Bai <lkevin957@gmail.com>
2021-06-13 16:17:30 -0700

    Add base promise

--- promises/promise.js
+++ promises/promise.js
@@ -1,19 +1,94 @@
-hi nick
+/*
+Goal - write the promise API
 
-WHOOO
+- then
+    - resolve
+    - reject
+- catch
+
+// async
+    callback that takes resolve reject
+
+    mysteryfunc
+    run the thing
+    return success / fail
+
+    here
+    const myPromise = new Promise(
+        (resolve, reject) => {
+            setTimeout(() => {
+                resolve('foo');
+            }, 300);
+        }
+    );
+*/
 
 
-class Promise {
-    
-    constructor() {
-
+class myPromise {
+    constructor(
+        asyncFunction,
+        resolved,
+        rejected,
+    ) {
+        try {
+            asyncFunction(this.resolve, this.reject);
+        } catch(error) {
+            this.reject(error);
+        }
+        this.pending = true;
+        this.resolved = resolved || false;
+        this.rejected = rejected || false;
     }
 
-    then() {
-
+    resolve(resolved) {
+        this.success = resolved;
     }
 
-    catch() {
+    reject(rejected) {
+        this.pending = false;
+        this.rejected = rejected;
+    }
 
+    then(actionFunc) {
+        if (resolved) {
+            actionFunc(resolved);
+        } 
+        return this;
+    }
+
+    catch(actionFunc) {
+        if (rejected) {
+            actionFunc(rejected);
+        }
+        return this;
     }
 }
+
+const examplePromise = new 
+    myPromise(() => setTimeout(0, console.log('hello')))
+    .then((resolve) => console.log(resolve))
+    .catch()
+;
+
+const mymyPromise = 
+    new myPromise(
+        (resolve, reject) => {
+            const asyncAction = fetch('someURL.io');
+            resolve(asyncAction);
+        }
+    ) // now we have an instance of myPromise, with the asyncAction running in the background
+    .then(
+        (arg) => console.log(arg))
+    ).this;
+
+    // finished running, returning the same instance of myPromise
+
+
+
+// const PromiseInstance = (asyncFunction){
+
+// }
+
+// const Promise = (asyncFunction) => {
+//     return PromiseInstance(asyncFunction);
+// }
