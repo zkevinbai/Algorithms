bc203df4fc761fa399e581ab8e2ab99d602b6677
Kevin Bai <lkevin957@gmail.com>
2022-06-24 00:01:44 -0400

    jewels and stones

--- LeetCode/771-JewelsAndStones.js
+++ LeetCode/771-JewelsAndStones.js
@@ -1,11 +1,40 @@
 var numJewelsInStones = function (J, S) {
-    let count = 0;
+    //     iterate through stones
+    const stoneCount = {};
+
     for (let i = 0; i < S.length; i++) {
         const stone = S[i];
-        
-        if(J.includes(stone)){
-            count += 1;
+
+        if (stoneCount[stone]) {
+            stoneCount[stone] += 1
+        } else {
+            stoneCount[stone] = 1
         }
     }
-    return count;
+
+    let jewelCount = 0;
+
+    const jewels = J.split("");
+
+    jewels.forEach((jewel => {
+        const count = stoneCount[jewel]
+
+        if (count) {
+            jewelCount += count
+        }
+    }))
+
+    return jewelCount
 };
+
+// var numJewelsInStones = function (J, S) {
+//     let count = 0;
+//     for (let i = 0; i < S.length; i++) {
+//         const stone = S[i];
+        
+//         if(J.includes(stone)){
+//             count += 1;
+//         }
+//     }
+//     return count;
+// };
