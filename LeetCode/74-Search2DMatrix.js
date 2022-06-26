2480934aaf593e6407ee715aa47912321e1356e6
Kevin Bai <lkevin957@gmail.com>
2022-06-26 10:40:56 -0400

    Add alternative search method

--- LeetCode/74-Search2DMatrix.js
+++ LeetCode/74-Search2DMatrix.js
@@ -6,51 +6,71 @@
 */
 
 const searchMatrix = (matrix, target) => {
-    const matrixHeight = matrix.length;
 
-    const startingValues = matrix.map(
-        row => row[0]
-    )
+    let i = 0
+    let j = matrix[0].length - 1
 
-    let rowIndexToSearch = 0;
+    while (i < matrix.length && j >= 0) {
+        const currentValue = matrix[i][j];
 
-    if (matrix[rowIndexToSearch][0] > target) {
-        return false
-    }
-
-    for (let i = 0; i < startingValues.length; i++) {
-        const currentStartingValue = startingValues[i];
-        const nextStartingValue = startingValues[i + 1];
-
-        if (
-            currentStartingValue === target ||
-            nextStartingValue === target
-        ) {
+        if (currentValue === target) {
             return true;
-        }
-
-        if (
-            (currentStartingValue && nextStartingValue) &&
-            (currentStartingValue < target && nextStartingValue > target)
-        ) {
-            rowIndexToSearch = i
-        }
-
-        if (!nextStartingValue && currentStartingValue < target) {
-            rowIndexToSearch = i
-        }
-    }
-
-    const rowToSearch = matrix[rowIndexToSearch];
-
-    // console.log(rowToSearch)
-
-    for (let i = 1; i < rowToSearch.length; i++) {
-
-        if (rowToSearch[i] === target) {
-            return true;
+        } else if (currentValue > target) {
+            j--
+        } else if (currentValue, target) {
+            i++
         }
     }
 
     return false;
-};
+}
+
+// const searchMatrix = (matrix, target) => {
+//     const matrixHeight = matrix.length;
+
+//     const startingValues = matrix.map(
+//         row => row[0]
+//     )
+
+//     let rowIndexToSearch = 0;
+
+//     if (matrix[rowIndexToSearch][0] > target) {
+//         return false
+//     }
+
+//     for (let i = 0; i < startingValues.length; i++) {
+//         const currentStartingValue = startingValues[i];
+//         const nextStartingValue = startingValues[i + 1];
+
+//         if (
+//             currentStartingValue === target ||
+//             nextStartingValue === target
+//         ) {
+//             return true;
+//         }
+
+//         if (
+//             (currentStartingValue && nextStartingValue) &&
+//             (currentStartingValue < target && nextStartingValue > target)
+//         ) {
+//             rowIndexToSearch = i
+//         }
+
+//         if (!nextStartingValue && currentStartingValue < target) {
+//             rowIndexToSearch = i
+//         }
+//     }
+
+//     const rowToSearch = matrix[rowIndexToSearch];
+
+//     // console.log(rowToSearch)
+
+//     for (let i = 1; i < rowToSearch.length; i++) {
+
+//         if (rowToSearch[i] === target) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// };
