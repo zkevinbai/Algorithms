09ea1dec45763b144ccea604cd10897b9129b3c0
Kevin Bai <lkevin957@gmail.com>
2023-10-12 15:26:44 -0700

    Redo Group Anagrams

--- LeetCode/49-GroupAnagrams.js
+++ LeetCode/49-GroupAnagrams.js
@@ -1,10 +1,10 @@
 /*
 
+https://leetcode.com/problems/group-anagrams/description/
+
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
-
 
 Example 1:
 
@@ -18,7 +18,6 @@ Example 3:
 
 Input: strs = ["a"]
 Output: [["a"]]
-
 
 Constraints:
 
@@ -43,3 +42,20 @@ const groupAnagrams = (strings) => {
 
     return Object.values(anagrams);
 };
+
+// var groupAnagrams = (strings) => {
+//     const hash = []
+
+//     for (let i = 0; i < strings.length; i++) {
+//         const word = strings[i]
+//         const sortedWord = strings[i].split("").sort().join("")
+
+//         if (!hash[sortedWord]) {
+//             hash[sortedWord] = [word]
+//         } else {
+//             hash[sortedWord].push(word)
+//         }
+//     }
+
+//     return hash.values()
+// }
