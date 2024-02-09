c39b589aeb8c54200a54e5a3a2bd7346659a15c2
Kevin Bai <lkevin957@gmail.com>
2024-02-09 10:09:01 -0800

    valid parens

--- 20-ValidParenthesis.js
+++ 20-ValidParenthesis.js
@@ -11,7 +11,37 @@ Open brackets must be closed in the correct order.
 // add values to stack, and pop them off as the pairs show up
 // if the stack is empty at the end of the iteration, the string is valid
 
+
 const isValid = (string) => {
+    const array = string.split('')
+
+    const bracketPairs = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
+    }
+
+    const stack = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const value = array[i]
+        const stackTop = stack[stack.length - 1]
+
+        // console.log({value, stack, stackTop})
+
+        if (stackTop && bracketPairs[value] === stackTop) {
+            stack.pop()
+        } else {
+            stack.push(value)
+        }
+
+        // console.log(stack)
+    }
+
+    return stack.length === 0
+}
+
+const isValid2021 = (string) => {
     const array = string.split("");
 
     const leftBracketsPairs = {
