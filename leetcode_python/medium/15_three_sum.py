82be4c931d5dae12e1f79773f800d45391b36ff4
Kevin Bai <lkevin957@gmail.com>
2025-03-22 20:35:59 -0700

    3sum

---
+++ leetcode_python/medium/15_three_sum.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        nums.sort()
+        res = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total > 0:
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                elif total == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return res
