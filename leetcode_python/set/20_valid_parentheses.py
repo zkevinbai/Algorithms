25c93b080c0c3f4bec10f1557b11d504eabaa697
Kevin Bai <lkevin957@gmail.com>
2025-03-24 23:21:18 -0700

    valid parens

--- leetcode_python/set/20_valid_parentheses.py
+++ leetcode_python/set/20_valid_parentheses.py
@@ -6,13 +6,18 @@ class Solution(object):
         """
 
         stack = []
-        mapping = {'(': ')', '[': ']', '{': '}'}  # Open → Close
 
-        for char in s:
-            if char in mapping:  # If it's an opening bracket
-                stack.append(mapping[char])  # Push expected closing bracket
-            else:  # It's a closing bracket
-                if not stack or stack.pop() != char:
-                    return False  # Either stack is empty or mismatch found
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
 
-        return not stack  # Stack should be empty if all brackets matched
+        for b in s:
+            if b in pairs:
+                if not stack or stack.pop() != pairs[b]:
+                    return False
+            else:
+                stack.append(b)
+
+        return not stack
