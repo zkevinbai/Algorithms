9d696771cf6aab552444744fe92f448ae63ae723
Kevin Bai <lkevin957@gmail.com>
2025-03-24 17:31:04 -0700

    3 sum

--- leetcode_python/two_pointer/15_three_sum.py
+++ leetcode_python/two_pointer/15_three_sum.py
@@ -6,30 +6,35 @@ class Solution(object):
         """
 
         nums.sort()
-        res = []
+
+        solutions = []
 
         for i in range(len(nums) - 2):
+
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
             left, right = i + 1, len(nums) - 1
 
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
+                combo = nums[i] + nums[left] + nums[right]
 
-                if total > 0:
-                    right -= 1
-                elif total < 0:
-                    left += 1
-                elif total == 0:
-                    res.append([nums[i], nums[left], nums[right]])
+                if combo == 0:
+                    solutions.append([nums[i], nums[left], nums[right]])
 
                     left += 1
                     right -= 1
 
                     while left < right and nums[left] == nums[left - 1]:
                         left += 1
+
                     while left < right and nums[right] == nums[right + 1]:
                         right -= 1
 
-        return res
+                elif combo > 0:
+                    right -= 1
+
+                elif combo < 0:
+                    left += 1
+
+        return solutions
