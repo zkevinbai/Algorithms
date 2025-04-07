f35c7fa4e122df9e46f2ed39107abf01cce48502
Kevin Bai <lkevin957@gmail.com>
2025-04-06 17:11:54 -0700

    left and right

--- leetcode_python/set/22_generate_parentheses.py
+++ leetcode_python/set/22_generate_parentheses.py
@@ -6,16 +6,16 @@ class Solution(object):
         """
         result = []
 
-        def backtrack(current, open_count, close_count):
+        def backtrack(current, left, right):
             if len(current) == 2 * n:  # Base case: When we've placed all n pairs
                 result.append(current)
                 return
 
-            if open_count < n:  # If we can still place an open bracket, do it
-                backtrack(current + "(", open_count + 1, close_count)
+            if left < n:  # If we can still place an open bracket, do it
+                backtrack(current + "(", left + 1, right)
 
-            if close_count < open_count:  # If we can place a close bracket, do it
-                backtrack(current + ")", open_count, close_count + 1)
+            if right < left:  # If we can place a close bracket, do it
+                backtrack(current + ")", left, right + 1)
 
         backtrack("", 0, 0)
         return result
